06f6c050faac6045a81026bb9b9d31a413cb9d39
HieuTong <hieuminhtongle20111998@gmail.com>
2021-02-22 22:28:59 +0700

    camera

--- LearningInstagram/Camera/PreviewPhotoContainerView.swift
+++ LearningInstagram/Camera/PreviewPhotoContainerView.swift
@@ -5,4 +5,104 @@
 //  Created by HieuTong on 2/22/21.
 //
 
-import Foundation
+import UIKit
+import Photos
+
+class PreviewPhotoContainerView: UIView {
+    
+    let previewImageView: UIImageView = {
+        let iv = UIImageView()
+        return iv
+    }()
+    
+    let cancleButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "cancel_shadow").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleCancel), for: .touchUpInside)
+        return button
+    }()
+    
+    let saveButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "save_shadow").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleSave), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    @objc func handleSave() {
+        print("Handling save ...")
+        
+        guard let previewImage = previewImageView.image else { return }
+        let library = PHPhotoLibrary.shared()
+        library.performChanges {
+            PHAssetChangeRequest.creationRequestForAsset(from: previewImage)
+        } completionHandler: { (success, err) in
+            if let err = err {
+                print("Failed to save image to photo library: ", err)
+                return
+            }
+            
+            print("Successfully saved image to library")
+            
+            DispatchQueue.main.async {
+                let saveLabel = UILabel()
+                saveLabel.text = "Saved successfully"
+                saveLabel.font = .boldSystemFont(ofSize: 18)
+                saveLabel.textColor = .white
+                saveLabel.numberOfLines = 0
+                saveLabel.backgroundColor = UIColor(white: 0, alpha: 0.3)
+                saveLabel.textAlignment = .center
+                
+                saveLabel.frame = CGRect(x: 0, y: 0, width: 150, height: 80)
+                saveLabel.center = self.center
+                
+                self.addSubview(saveLabel)
+                
+                saveLabel.layer.transform = CATransform3DMakeScale(0, 0, 0)
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut) {
+                    
+                    saveLabel.layer.transform = CATransform3DMakeScale(1, 1, 1)
+                    
+                } completion: { (completed) in
+                    //completed
+                    
+                    UIView.animate(withDuration: 0.5, delay: 0.75, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut) {
+                        
+                        saveLabel.layer.transform = CATransform3DMakeScale(0.1, 0.1, 0.1)
+                        saveLabel.alpha = 0
+                        
+                    } completion: { (_) in
+                        saveLabel.removeFromSuperview()
+                    }
+
+                }
+
+            }
+        }
+
+        
+    }
+    
+    @objc func handleCancel() {
+        self.removeFromSuperview()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .yellow
+        
+        addSubview(previewImageView)
+        previewImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        addSubview(cancleButton)
+        cancleButton.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 12, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 50, height: 50)
+        
+        addSubview(saveButton)
+        saveButton.anchor(top: nil, left: leftAnchor, bottom: bottomAnchor, right: nil, paddingTop: 0, paddingLeft: 24, paddingBottom: 24, paddingRight: 0, width: 50, height: 50)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
